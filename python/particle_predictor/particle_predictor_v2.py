25e896c0668b17b65547b82934bfea21e6b40029
KiranArun <kironni@gmail.com>
2017-06-12 10:10:35 +0100

    updated paython file to match the notebook

--- python/particle_predictor/particle_predictor_v2.py
+++ python/particle_predictor/particle_predictor_v2.py
@@ -2,6 +2,8 @@
 import tensorflow as tf
 import numpy as np
 
+
+
 # setup the parameters
 # number of input values
 vals = 3
@@ -9,21 +11,23 @@ vals = 3
 max_answer = 40
 # number of different M's, biggest gradient will fit in frame
 gradients = max_answer/(vals)+1
-iterations = 11000
-learning_rate = 0.3
+iterations = 4400
+learning_rate = 0.5
 
 # I am using a GPU
 # this line limits memory usage of the GPU to 0.25 when session is created
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
+
+
 
 # defining function to make training data
 def training_data():
     
-    n = 0
+    M = 0
     rows = 1
     # array which we are using as our x values 
     # in equation of linear line, y = Mx
-    # it includes 1 extra value as this will be used as our labal
+    # it includes 1 extra value as this will be used as our label
     X = np.arange(vals+1).astype(np.int32)
     # empty array to write our training data to
     Y = np.array([])
@@ -31,14 +35,14 @@ def training_data():
     # loop so it cycles through every gradient
     for i in range(gradients):
         
-        Y = np.append(Y, X*n).reshape(rows,vals+1)
+        Y = np.append(Y, X*M).reshape(rows,vals+1)
             
         # increase number of rows to reshape it
         rows += 1
         
         # increase gradient by 1
-        n+=1
-    
+        M+=1
+     
     Y = Y.astype(np.int32)
     # return the training data
     # and number of lines to learn
@@ -48,7 +52,8 @@ def training_data():
 print(training_data())
 
 
-# data = training data
+
+# training_line_data = training data in numbers
 # training lines = number of different lines
 training_line_data,training_lines = training_data()
 
@@ -65,33 +70,32 @@ full_length = length*vals
 def set_data():
     
     # there are two arrays, one for training data and one for labels
-    in_data = np.zeros([training_lines,vals,length])
+    # input_data  is the converted traiing data
+    input_data = np.zeros([training_lines,vals,length])
     # the labels will be one-hot arrays
-    lab = np.zeros([training_lines,1,gradients])
+    labels = np.zeros([training_lines,1,gradients])
     
     # this sets the values specified in the training data to one
     for i in range(training_lines):
         # we need to set each individual input value
         for a in range(vals):
             # set the value to a 1
-            in_data[i,a,training_line_data[i,a]] = 1
+            input_data[i,a,training_line_data[i,a]] = 1
             
         # set the label value to a 1
-        lab[i,0,training_line_data[i,vals-(vals-1)]] = 1
+        labels[i,0,training_line_data[i,vals-(vals-1)]] = 1
         
     # here, we reshape it tto the full length 1d array
-    in_data = in_data.reshape(training_lines,1,full_length)
+    input_data = input_data.reshape(training_lines,1,full_length)
     
     # return the data and labels
-    return(in_data,lab)    
+    return(input_data,labels)    
 
 # print converted data
 #print(set_data())
 
 
-###################################################################################
-###################################################################################
-
+# In[25]:
 
 # we define the weights, biases and inputs
 # this will be input training data
@@ -107,14 +111,11 @@ y_ = tf.placeholder(tf.float32, [None, gradients])
 
 # configure the loss function, using cross entropy
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
 # define the optimizer
 # AdagradOptimizer works much better than GradientDescentOptimizer
 #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 train_step = tf.train.AdagradOptimizer(learning_rate).minimize(cross_entropy)
-
-
-###################################################################################
-###################################################################################
 
 
 # create interactive session using the GPU line for above
@@ -137,16 +138,10 @@ for _ in range(iterations):
     # run the training optimizer
     sess.run(train_step, feed_dict={x: x_data, y_: y_data})
 
-    # print steps and error 20 times in total
+    # print steps and error 10 times in total
     if _ % (iterations/20) == 0:
         print 'step', _, 'out of', iterations
         print 'error =', sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data})
-
-
-
-
-###################################################################################
-###################################################################################
 
 
 
@@ -164,25 +159,27 @@ for i in range(vals):
 test_line = test_line.astype(np.int32)
 # record the last value of the points
 # this is needed because we have only worked out the gradient not the bias
-l_val = test_line[-1]
+last_val = test_line[-1]
 # show line that user has inputed
 print(test_line)
 
 # function to convert data into training data format
 def test_model():
     # first value of the points to take away the b later
-    f_value = test_line[0]
+    global test_line
+    first_value = test_line[0]
     
     # take away the first value so its only left with the difference between
     # the points and predicts y = Mx , not y = Mx+ b
-	test_line = np.subtract(test_line,f_value)
+    test_line = np.subtract(test_line,first_value)
+
 
     input_array = np.array([])
     # for each input value, write to the input array
     # with a 1 in the position
     for a in range(vals):
         test_array = np.zeros([1,length])
-        test_array[0,test_line[a]] = 1
+        test_array[0][test_line[a]] = 1
         # we write to array and shape so new line for each test value
         input_array = np.append(input_array, test_array).reshape([1,(a+1)*length])
         
@@ -195,32 +192,32 @@ def test_model():
 
 # run the function to get output
 # using new weights and biases
-in_array = test_model()
-probs = sess.run(y, feed_dict={x:in_array})
+input_array = test_model()
+probs = sess.run(y, feed_dict={x:input_array})
 # now we have found the predicted gradient,
 # we add the last value to get the next value
-learnt_ans = np.argmax(probs)+l_val
+learnt_ans = np.argmax(probs)+last_val
 # "true" answer, should work with all linear lines
 # this numerically works out hte gradient by findingthe difference,
 # then also adds the last value
-answer = (test_line[1]-test_line[0])+l_val
+answer = (test_line[1]-test_line[0])+last_val
 
 if answer == learnt_ans:
     print 'Correct'
     
     # print learnt answer and it probability
     print '\nLearnt answer =', learnt_ans
-    print 'Probaility of Gradient', np.argmax(probs), '=', probs[0,learnt_ans-l_val]
+    print 'Probaility of Gradient', np.argmax(probs), '=', probs[0][learnt_ans-last_val]
 else:
     print 'Wrong'
     
     # print learnt answer and it probability
     print '\nLearnt answer =', learnt_ans
-    print 'Probaility of Gradient', np.argmax(probs), '=', probs[0,learnt_ans-l_val]
+    print 'Probaility of Gradient', np.argmax(probs), '=', probs[0][learnt_ans-last_val]
     
     # print true answer and its probabilities
     print '\nNumerical answer =', answer
-    print 'Probability of Gradient', test_line[1]-test_line[0], '=', probs[0,answer-l_val]
+    print 'Probability of Gradient', test_line[1]-test_line[0], '=', probs[0][answer-last_val]
     
 
 # print the probabilities
